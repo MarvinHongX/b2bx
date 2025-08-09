e56f5e7357898006b8e44ad2c8e3bc46fce1ca4d
MarvinHongX <marvinhongx@gmail.com>
2025-08-09 19:40:33 +0900

    Feature Enhancement

--- composables/useImg.js
+++ composables/useImg.js
@@ -1,193 +1,97 @@
-export const useImg = () => {
-    const logoUrl = computed(() => {
-        return `/layout/images/logo.png`;
-    });
-    const grayLogoUrl = computed(() => {
-        return `/layout/images/grayLogo.png`;
-    });
-    const phoneUrl = computed(() => {
-        return `/layout/images/phone.svg`;
-    });
-    const lightPhoneUrl = computed(() => {
-        return `/layout/images/lightPhone.svg`;
-    });
-    const moreUrl = computed(() => {
-        return `/layout/images/more.svg`;
-    });
-    const gotoUrl = computed(() => {
-        return `/layout/images/side/goto.svg`;
-    });
-    const kakaoUrl = computed(() => {
-        return `/layout/images/side/kakao.svg`;
-    });
-    const skyLineUrl = computed(() => {
-        return `/layout/images/header/skyLine.svg`;
-    });
-    const bannerNextUrl = computed(() => {
-        return `/layout/images/banner/bannerNext.svg`;
-    });
-    const bannerPreviousUrl = computed(() => {
-        return `/layout/images/banner/bannerPrevious.svg`;
-    });
-    const banner0Url = computed(() => {
-        return `/layout/images/banner/banner0.png`;
-    });
-    const banner1Url = computed(() => {
-        return `/layout/images/banner/banner1.png`;
-    });
-    const banner1SmallUrl = computed(() => {
-        return `/layout/images/banner/banner1small.png`;
-    });
-    const callUrl = computed(() => {
-        return `/layout/images/side/call.svg`;
-    });
-    const features1Url = computed(() => {
-        return `/layout/images/features/features1.svg`;
-    });
-    const features2Url = computed(() => {
-        return `/layout/images/features/features2.svg`;
-    });
-    const features3Url = computed(() => {
-        return `/layout/images/features/features3.svg`;
-    });
-    const features4Url = computed(() => {
-        return `/layout/images/features/features4.svg`;
-    });
-    const rentalProcess1Url = computed(() => {
-        return `/layout/images/rentalProcess/rentalProcess1.svg`;
-    });
-    const rentalProcess2Url = computed(() => {
-        return `/layout/images/rentalProcess/rentalProcess2.svg`;
-    });
-    const rentalProcess3Url = computed(() => {
-        return `/layout/images/rentalProcess/rentalProcess3.svg`;
-    });
-    const rentalProcess4Url = computed(() => {
-        return `/layout/images/rentalProcess/rentalProcess4.svg`;
-    });
-    const rentalProcess5Url = computed(() => {
-        return `/layout/images/rentalProcess/rentalProcess5.svg`;
-    });
-    const rentalProcess6Url = computed(() => {
-        return `/layout/images/rentalProcess/rentalProcess6.svg`;
-    });
-    const rentalProcessNextUrl = computed(() => {
-        return `/layout/images/rentalProcess/rentalProcessNext.svg`;
-    });
-    const line1Url = computed(() => {
-        return `/layout/images/directions/line1.png`;
-    });
-    const line7Url = computed(() => {
-        return `/layout/images/directions/line7.png`;
-    });
-    const lineAirUrl = computed(() => {
-        return `/layout/images/directions/lineAir.svg`;
-    });
-    const lineBUrl = computed(() => {
-        return `/layout/images/directions/lineB.png`;
-    });
-    const lineGUrl = computed(() => {
-        return `/layout/images/directions/lineG.png`;
-    });
-    const printUrl = computed(() => {
-        return `/layout/images/directions/print.svg`;
-    });
-    const ceoUrl = computed(() => {
-        return `/layout/images/about/ceo.svg`;
-    });
-    const companyUrl = computed(() => {
-        return `/layout/images/about/company.png`;
-    });
-    const company2Url = computed(() => {
-        return `/layout/images/about/company2.svg`;
-    });
-    const companyDocUrl = computed(() => {
-        return `/layout/images/about/companyDoc.svg`;
-    });
-    const companyApprovalUrl = computed(() => {
-        return `/layout/images/about/companyApproval.svg`;
-    });
-    const webtoon1Url = computed(() => {
-        return `/layout/images/guide/webtoon1.jpg`;
-    });
-    const webtoon2Url = computed(() => {
-        return `/layout/images/guide/webtoon2.jpg`;
-    });
-    const puppyKindergarten1Url = computed(() => {
-        return `/layout/images/cases/puppyKindergarten1.jpg`;
-    });
-    const puppyKindergarten2Url = computed(() => {
-        return `/layout/images/cases/puppyKindergarten2.jpg`;
-    });
-    const puppyKindergarten3Url = computed(() => {
-        return `/layout/images/cases/puppyKindergarten3.jpg`;
-    });
-    const puppyKindergarten4Url = computed(() => {
-        return `/layout/images/cases/puppyKindergarten4.jpg`;
-    });
-    const mealkit1Url = computed(() => {
-        return `/layout/images/cases/mealkit1.jpg`;
-    });
-    const mealkit2Url = computed(() => {
-        return `/layout/images/cases/mealkit2.jpg`;
-    });
-    const mealkit3Url = computed(() => {
-        return `/layout/images/cases/mealkit3.jpg`;
-    });
-    const mealkit4Url = computed(() => {
-        return `/layout/images/cases/mealkit4.jpg`;
-    });
-    const modalLogoUrl = computed(() => {
-        return `/layout/images/modal/modalLogo.svg`;
-    });
-    return {
-        logoUrl,
-        grayLogoUrl,
-        phoneUrl,
-        lightPhoneUrl,
-        moreUrl,
-        gotoUrl,
-        kakaoUrl,
-        skyLineUrl,
-        bannerNextUrl,
-        bannerPreviousUrl,
-        banner0Url,
-        banner1Url,
-        banner1SmallUrl,
-        callUrl,
-        features1Url,
-        features2Url,
-        features3Url,
-        features4Url,
-        rentalProcess1Url,
-        rentalProcess2Url,
-        rentalProcess3Url,
-        rentalProcess4Url,
-        rentalProcess5Url,
-        rentalProcess6Url,
-        rentalProcessNextUrl,
-        line1Url,
-        line7Url,
-        lineAirUrl,
-        lineBUrl,
-        lineGUrl,
-        printUrl,
-        ceoUrl,
-        companyUrl,
-        company2Url,
-        companyDocUrl,
-        companyApprovalUrl,
-        webtoon1Url,
-        webtoon2Url,
-        puppyKindergarten1Url,
-        puppyKindergarten2Url,
-        puppyKindergarten3Url,
-        puppyKindergarten4Url,
-        mealkit1Url,
-        mealkit2Url,
-        mealkit3Url,
-        mealkit4Url,
-        modalLogoUrl
-    };
-};
+import logoUrl from '/assets/images/logo.webp';
+import grayLogoUrl from '/assets/images/grayLogo.webp';
+import phoneUrl from '/assets/images/phone.svg';
+import lightPhoneUrl from '/assets/images/lightPhone.svg';
+import moreUrl from '/assets/images/more.svg';
+import gotoUrl from '/assets/images/side/goto.svg';
+import kakaoUrl from '/assets/images/side/kakao.svg';
+import skyLineUrl from '/assets/images/header/skyLine.svg';
+import bannerNextUrl from '/assets/images/banner/bannerNext.svg';
+import bannerPreviousUrl from '/assets/images/banner/bannerPrevious.svg';
+import banner0Url from '/assets/images/banner/banner0.webp';
+import banner1Url from '/assets/images/banner/banner1.webp';
+import banner1SmallUrl from '/assets/images/banner/banner1small.webp';
+import callUrl from '/assets/images/side/call.svg';
+import features1Url from '/assets/images/features/features1.svg';
+import features2Url from '/assets/images/features/features2.svg';
+import features3Url from '/assets/images/features/features3.svg';
+import features4Url from '/assets/images/features/features4.svg';
+import rentalProcess1Url from '/assets/images/rentalProcess/rentalProcess1.svg';
+import rentalProcess2Url from '/assets/images/rentalProcess/rentalProcess2.svg';
+import rentalProcess3Url from '/assets/images/rentalProcess/rentalProcess3.svg';
+import rentalProcess4Url from '/assets/images/rentalProcess/rentalProcess4.svg';
+import rentalProcess5Url from '/assets/images/rentalProcess/rentalProcess5.svg';
+import rentalProcess6Url from '/assets/images/rentalProcess/rentalProcess6.svg';
+import rentalProcessNextUrl from '/assets/images/rentalProcess/rentalProcessNext.svg';
+import line1Url from '/assets/images/directions/line1.webp';
+import line7Url from '/assets/images/directions/line7.webp';
+import lineAirUrl from '/assets/images/directions/lineAir.svg';
+import lineBUrl from '/assets/images/directions/lineB.webp';
+import lineGUrl from '/assets/images/directions/lineG.webp';
+import printUrl from '/assets/images/directions/print.svg';
+import ceoUrl from '/assets/images/about/ceo.svg';
+import companyUrl from '/assets/images/about/company.webp';
+import company2Url from '/assets/images/about/company2.svg';
+import companyDocUrl from '/assets/images/about/companyDoc.svg';
+import companyApprovalUrl from '/assets/images/about/companyApproval.svg';
+import webtoon1Url from '/assets/images/guide/webtoon1.webp';
+import webtoon2Url from '/assets/images/guide/webtoon2.webp';
+import puppyKindergarten1Url from '/assets/images/cases/puppyKindergarten1.webp';
+import puppyKindergarten2Url from '/assets/images/cases/puppyKindergarten2.webp';
+import puppyKindergarten3Url from '/assets/images/cases/puppyKindergarten3.webp';
+import puppyKindergarten4Url from '/assets/images/cases/puppyKindergarten4.webp';
+import mealkit1Url from '/assets/images/cases/mealkit1.webp';
+import mealkit2Url from '/assets/images/cases/mealkit2.webp';
+import mealkit3Url from '/assets/images/cases/mealkit3.webp';
+import mealkit4Url from '/assets/images/cases/mealkit4.webp';
+import modalLogoUrl from '/assets/images/modal/modalLogo.svg';
 
+export const useImg = () => ({
+    logoUrl,
+    grayLogoUrl,
+    phoneUrl,
+    lightPhoneUrl,
+    moreUrl,
+    gotoUrl,
+    kakaoUrl,
+    skyLineUrl,
+    bannerNextUrl,
+    bannerPreviousUrl,
+    banner0Url,
+    banner1Url,
+    banner1SmallUrl,
+    callUrl,
+    features1Url,
+    features2Url,
+    features3Url,
+    features4Url,
+    rentalProcess1Url,
+    rentalProcess2Url,
+    rentalProcess3Url,
+    rentalProcess4Url,
+    rentalProcess5Url,
+    rentalProcess6Url,
+    rentalProcessNextUrl,
+    line1Url,
+    line7Url,
+    lineAirUrl,
+    lineBUrl,
+    lineGUrl,
+    printUrl,
+    ceoUrl,
+    companyUrl,
+    company2Url,
+    companyDocUrl,
+    companyApprovalUrl,
+    webtoon1Url,
+    webtoon2Url,
+    puppyKindergarten1Url,
+    puppyKindergarten2Url,
+    puppyKindergarten3Url,
+    puppyKindergarten4Url,
+    mealkit1Url,
+    mealkit2Url,
+    mealkit3Url,
+    mealkit4Url,
+    modalLogoUrl,
+});
